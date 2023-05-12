9156fdbbe4af885893f73c6e2eacc947d68fd83a
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2023-05-11 22:25:31 -0300

    aula 2

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
 const http = require("http");
 const url = require('url');
+const crypto = require('crypto');
 
 const host = 'localhost';
 const port = 8000;
 
 const requestHtml = function (page) {
-    return "<html><body>conteudo</body></html>";
+    if(page === "/page1") {
+        return "<html><body>Page 1 v2</body></html>";
+    } 
+    return "<html><body>Page 2</body></html>";
+    
+    //return `<html><body>Page ${page}</body></html>`;
 }
 
-const requestListener = function (req, res) {
+function generateHash(pageHtml) {
+    return crypto.createHash('md5').update(pageHtml).digest('hex');
+}
+
+function requestListener (req, res) {
     const parsedUrl = url.parse(req.url, true); 
     const path = parsedUrl.pathname;
     console.log(path);
-    res.writeHead(200);
-    res.end(requestHtml("page1"));
+ 
+    const pageHtml = requestHtml(path);
+    let hashPage = generateHash(pageHtml)
+    if(hashPage === req.headers["if-none-match"]) {
+        res.writeHead(304);
+        res.end();
+    }
+
+    //res.writeHead(200, {"cache-control": "no-store"})
+    //res.writeHead(200, {"cache-control": "max-age=0"})
+    res.writeHead(200, {
+        "cache-control": "max-age=0, must-revalidate",
+        "etag": hashPage
+    })
+   
+    res.end(pageHtml);
 };
 
 //cria o servidor
